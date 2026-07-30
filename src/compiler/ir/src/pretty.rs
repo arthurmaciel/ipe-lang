2e8d8979e3e9936228d275cb216e29ffb32746b8
Arthur Maciel <arthurmaciel@gmail.com>
2026-07-30 11:30:40 -0300

    fix(ir): cover Url_* kernels in the KernelFn display-name printer

--- src/compiler/ir/src/pretty.rs
+++ src/compiler/ir/src/pretty.rs
@@ -1364,6 +1364,15 @@ const fn kernel_name(kernel: KernelFn) -> &'static str {
         KernelFn::CryptoChacha20DecryptKey => "Crypto.chacha20DecryptKey",
         KernelFn::EmailAddressParse => "EmailAddress.parse",
         KernelFn::EmailAddressToString => "EmailAddress.toString",
+        KernelFn::UrlFromString => "Url.fromString",
+        KernelFn::UrlToString => "Url.toString",
+        KernelFn::UrlScheme => "Url.scheme",
+        KernelFn::UrlHost => "Url.host",
+        KernelFn::UrlPort => "Url.port",
+        KernelFn::UrlPath => "Url.path",
+        KernelFn::UrlQuery => "Url.query",
+        KernelFn::UrlFragment => "Url.fragment",
+        KernelFn::UrlBuildQuery => "Url.buildQuery",
     }
 }
 
